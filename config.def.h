5ce850014dba174aca5cf23280f0acdb15835e57
Florent <f@linguenheld.fr>
2023-04-18 13:21:47 +0200

    Add font

--- config.def.h
+++ config.def.h
@@ -13,6 +13,18 @@ static char *font2[] = {
     "IPAGothic:pixelsize=15:antialias=true:autohint=true", // Japanese
     "Symbola:pixelsize=15:antialias=true:autohint=true" // Unicode
 };
+<<<<<<<
+=======
+ * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
+ */
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
+>>>>>>>
 static int borderpx = 2;
 
 /*
