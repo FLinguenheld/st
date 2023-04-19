f47d991e7858d89eb6d5cbb5f49cce8565e59dca
Florent <f@linguenheld.fr>
2023-04-19 11:38:35 +0200

    Add external ref

--- config.def.h
+++ config.def.h
@@ -201,6 +201,16 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+/* External pipe URL */
+static char *openurlcmd[] = { "/bin/sh", "-c",
+	"sed 's/.*│//g' | tr -d '\n' | grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./@&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)'| uniq | sed 's/^www./http:\\/\\/www\\./g' | dmenu -w $(xdotool getactivewindow) -i -p 'Follow which url?' -l 10 | xargs -r xdg-open",
+	"externalpipe", NULL };
+
+static char *copyurlcmd[] = { "/bin/sh", "-c",
+	"sed 's/.*│//g' | tr -d '\n' | grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./@&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)' | uniq | sed 's/^www./http:\\/\\/www\\./g' | dmenu -w $(xdotool getactivewindow) -i -p 'Copy which url?' -l 10 | tr -d '\n' | xclip -selection clipboard",
+	"externalpipe", NULL };
+
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -223,6 +233,9 @@ static Shortcut shortcuts[] = {
 
 	{ XK_ANY_MOD,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ XK_ANY_MOD,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+
+    { TERMMOD,          	XK_U,           externalpipe,   {.v = openurlcmd } },
+	{ TERMMOD,          	XK_Y,           externalpipe,   {.v = copyurlcmd } },
 };
 
 /*
