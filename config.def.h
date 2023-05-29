34707f5d8d9e2e51766fbbe99a100f427d1b27c2
Florent <f@linguenheld.fr>
2023-05-29 21:03:36 +0200

    Restart

--- config.def.h
+++ config.def.h
@@ -1,23 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
-
 /*
  * appearance
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-// static char *font = "mono:pixelsize=15:antialias=true:autohint=true";
-static char *font = "JetBrains Mono Nerd Font:pixelsize=15:antialias=true:autohint=true";
-// static char *font = "FiraCode Nerd Font:pixelsize=15:antialias=true:autohint=true";
-// static char *font = "FuraMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-    "Hack Nerd Font:pixelsize=15:antialias=true:autohint=true", // Powerline
-    "Symbola:pixelsize=15:antialias=true:autohint=true" // Unicode
-};
-
-
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -106,43 +94,45 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
-/* Catpuccin Macchiato */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#494D64",
-	"#ED8796",
-	"#A6DA95",
-	"#EED49F",
-	"#8AADF4",
-	"#F5BDE6",
-	"#8BD5CA",
-	"#B8C0E0",
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
 	/* 8 bright colors */
-	"#5B6078",
-	"#ED8796",
-	"#A6DA95",
-	"#EED49F",
-	"#8AADF4",
-	"#F5BDE6",
-	"#8BD5CA",
-	"#A5ADCB",
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
-[256] = "#CAD3F5", /* default foreground colour */
-[257] = "#24273A", /* default background colour */
-[258] = "#F4DBD6", /*575268*/
+	[255] = 0,
 
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 };
 
 
 /*
+ * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 256;
-unsigned int defaultbg = 257;
-unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 258;
-
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
@@ -186,9 +176,6 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1} },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1} },
-
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -200,51 +187,20 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
-/* External pipe */
-static char *openurlcmd[] = { "/bin/sh", "-c",
-	"sed 's/.*│//g' | tr -d '\n' | grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./@&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)'| uniq | sed 's/^www./http:\\/\\/www\\./g' | dmenu -w $(xdotool getactivewindow) -i -p 'Follow which url?' -l 10 | xargs -r xdg-open",
-	"externalpipe", NULL };
-
-static char *copyurlcmd[] = { "/bin/sh", "-c",
-	"sed 's/.*│//g' | tr -d '\n' | grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./@&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)' | uniq | sed 's/^www./http:\\/\\/www\\./g' | dmenu -w $(xdotool getactivewindow) -i -p 'Copy which url?' -l 10 | tr -d '\n' | xclip -selection clipboard -r",
-	"externalpipe", NULL };
-
-static char *copypwd[] = { "/bin/sh", "-c",
-    "pidof $SHELL | xargs pwdx | awk '{print $2}' | uniq | dmenu -p 'Copy which dirctory?' -l 10 | xclip -selection clipboard -r",
-    "externalpipe", NULL };
-
-static char *copyemoji[] = { "/bin/sh", "-c",
-    // "cat ~/.config/st/emojis.txt | sort -i | dmenu -p 'Select one' -l 15 | awk '{print $1}' | xclip -selection clipboard -r",
-    "cat ~/.config/st/emojis.txt | dmenu -p 'Select one' -l 15 | awk '{print $1}' | xclip -selection clipboard -r",
-    "externalpipe", NULL };
-
-
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-
-    { TERMMOD,              XK_plus,            zoom,           {.f = +1} }, // +
-	{ TERMMOD,              XK_underscore,      zoom,           {.f = -1} }, // -
-	{ ControlMask,          XK_equal,           zoomreset,      {.f =  0} },
-
-	{ TERMMOD,              XK_C,               clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,               clippaste,      {.i =  0} },
-
-	// { TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	// { TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	// { TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-
-	{ XK_ANY_MOD,           XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ XK_ANY_MOD,           XK_Page_Down,   kscrolldown,    {.i = -1} },
-
-    { TERMMOD,              XK_U,           externalpipe,   {.v = openurlcmd } },
-	{ TERMMOD,              XK_Y,           externalpipe,   {.v = copyurlcmd } },
-	{ TERMMOD,              XK_P,           externalpipe,   {.v = copypwd } },
-
-	{ TERMMOD,              XK_E,           externalpipe,   {.v = copyemoji } },
+	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
+	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
+	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
+	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
+	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
 /*
